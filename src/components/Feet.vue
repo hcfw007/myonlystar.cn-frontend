d3e25a2d5364752e6645e1e2531b7e38bed92704
NickWang <brotherstyx@gmail.com>
2021-02-27 09:35:47 +0800

    feat: 🎸 footer content

--- src/components/Feet.vue
+++ src/components/Feet.vue
@@ -1,6 +1,13 @@
 <template>
   <a-layout-footer>
-    脚
+    <a-row>
+      <a-col :span="4">
+        Contact: <a href="mailTo: brotherstyx@gmail.com">brotherstyx@gmail.com</a>
+      </a-col>
+      <a-col :span="4">
+        Github: <a href="https://github.com/hcfw007">hcfw007</a>
+      </a-col>
+    </a-row>
   </a-layout-footer>
 </template>
 
